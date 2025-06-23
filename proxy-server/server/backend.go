f8a622ff56b9663189b362aba1a3f647cd44cfe7
Mark N <mark@nakmail.net>
2025-06-23 18:08:57 -0400

    Added Backend API interface

---
+++ proxy-server/server/backend.go
@@ -0,0 +1,15 @@
+package server
+
+import "proxyserver/pocketapi"
+import "time"
+
+type Backend interface {
+	Get(req pocketapi.GetRequest) (pocketapi.GetResponse, error)
+	ArticleText(url string) (pocketapi.ArticleTextResponse, error)
+	Add(url string, title string, time time.Time) error
+	Archive(itemID string, time time.Time) error
+	Unarchive(itemID string, time time.Time) error
+	Delete(itemID string, time time.Time) error
+	Favorite(itemID string, time time.Time) error
+	Unfavorite(itemID string, time time.Time) error
+}
